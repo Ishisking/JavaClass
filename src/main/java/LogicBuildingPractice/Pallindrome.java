d3c16c02c99276704133e35e59faf09d70d962d7
Ishaan <nashine.eshan1@gmail.com>
2026-01-09 14:21:50 +0530

    Jan 8 commits

--- src/main/java/LogicBuildingPractice/Pallindrome.java
+++ src/main/java/LogicBuildingPractice/Pallindrome.java
@@ -1,29 +1,30 @@
 package LogicBuildingPractice;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Pallindrome {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a Number :");
-		int n = sc.nextInt();
-		int on = n;
-		System.out.println("n =" + n + "and on =" + on);
-		int reverse = 0;
-		int lastdigit = 0;
-		while (n != 0) {
-			lastdigit = n % 10;
-			reverse = reverse * 10 + lastdigit;
-			n = n / 10;
-		}
-		System.out.println("Reverse of Number n is "+on);
-		if (reverse == on) {
-			System.out.println("Number on =" + on + " is a Pallindrome");
-		} else {
-			System.out.println("Number is not a Pallindrome ");
-		}
-		sc.close();
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter a Number :");
+//		int number = sc.nextInt();
+//		System.out.println("number =" + number);
+//		 Convert this number to String
+		int number = 899;
+
+		String originalNumberInString = Integer.toString(number);
+		String originalNumberInString2 = number + "";
+		// Reverse this String and for that i will be using special Class
+		// --StringBuilderClass
+		StringBuilder reverseStringBuilder = new StringBuilder(originalNumberInString);
+		String reverseString = reverseStringBuilder.reverse().toString();
+		System.out.println(reverseStringBuilder.reverse());
+// Compare 2 Strings
+		if (originalNumberInString.equals(reverseString)) {
+			System.out.println("Pallindrome");
+		} else
+			System.out.println("Its not a Pallindrome");
+		// sc.close();
 	}
 
 }
